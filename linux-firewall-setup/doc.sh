28765aa9ff3424afd06f51c76621e7f9056436a7
annavsr <anna_vsr@icloud.com>
2026-06-09 03:18:54 +0200

    Update doc.sh

--- linux-firewall-setup/doc.sh
+++ linux-firewall-setup/doc.sh
@@ -36,3 +36,46 @@ info "Architecture:      $(uname -m)"
 info "Uptime:            $(uptime -p)"
 info "Time:              $(date)"
 
+# -- 2. Benutzer auf dem System -- 
+
+section "2. Users on the system"
+
+info: "All Users with login-shell:"
+
+grep -E '/bin/bash|/bin/sh|/bin/zsh' /etc/passwd | while IFS=: read -r name _ uid gid _ _ _; do
+  if  [ "$uid" -eq 0 ]; then
+    alarm "ROOT-User: $name (UID=$uid)"
+  elif [ "$uid" -ge 1000 ]; then
+    ok "Normal User: $name (UID=$uid)"
+  else
+    warning "System-User with shell: $name (UID=$uid)"
+  fi
+done
+
+info "loged in User:"
+who
+
+# -- 3. Offene Ports & lauschende Dienste -- 
+
+section "3. Open ports & eavesdropping services"
+
+info "The following services are eavedr>opping on network connections:"
+
+if command -v ss &>/dev/null; then
+  ss -tulpn | grep LISTEN | while read -r line; do
+    port=$(echo "$line" | awk '{print $5}' | rev | cut -d: -f1 | rev)
+    case $port in
+        21) alarm "Port $port (FTP) - Unencrypted! Better to use SFTP | $line" ;;
+        23) alarm "Port $port (Telnet) - Very Unsafe! Please deactivate | $line" ;;
+        22) warning "Port $port (SSH) - Open. Secure with Key-Auth & Fail2ban" ;;
+        80) warning "Port $port (HTTP) - Unencrypted. Prefer HTTPS" ;;
+        443) ok "Port §port (HTTPS) - Encrypted, OK" ;;
+        3306) warning "Port $port (MySQL) - DB accessible from outside?" ;;
+        *) info "Port $port | $line" ;;
+    esac
+  done
+else
+  warning "ss not found, trying netstat..."
+  netstat -tulpn 2>/dev/null | grep LISTEN || info "No tool found."
+fi
+
